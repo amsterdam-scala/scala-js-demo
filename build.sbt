2531e7e64e8fa74f371da0e9c87af98c2a1eb737
Joost Heijkoop <joostheijkoop@gmail.com>
2015-03-05 20:42:01 +0100

    make it work with the dom through importing/calling jQuery

--- build.sbt
+++ build.sbt
@@ -4,3 +4,10 @@ name := "Scala.js Tutorial"
 
 scalaVersion := "2.11.5" // or any other Scala version >= 2.10.2
 
+scalaJSStage in Global := FastOptStage
+
+libraryDependencies += "org.scala-js" %%% "scalajs-dom" % "0.8.0"
+
+libraryDependencies += "be.doeraene" %%% "scalajs-jquery" % "0.8.0"
+
+skip in packageJSDependencies := false
--- src/main/scala/tutorial/webapp/TutorialApp.scala
+++ src/main/scala/tutorial/webapp/TutorialApp.scala
@@ -1,9 +1,23 @@
 package tutorial.webapp
 
 import scala.scalajs.js.JSApp
+import org.scalajs.dom
+import dom.document
+
+import scala.scalajs.js.annotation.JSExport
+import org.scalajs.jquery.jQuery
 
 object TutorialApp extends JSApp {
+  def appendPar(targetNode: dom.Node, text: String): Unit = {
+    jQuery("body").append(s"<p>$text</p>")
+  }
+
+  @JSExport
+  def addClickedMessage(): Unit = {
+    appendPar(document.body, "You clicked the button!")
+  }
+
   def main(): Unit = {
-    println("Hello world!")
+    appendPar(document.body, "Hello world!")
   }
 }
